537402c2950a48eb25f788aa8066ab743c6c20a5
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-03-05 12:44:54 +0700

    checking tailwind on deployment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
     return (
         <div css={style}>
-            <h1>Hello World</h1>
+            <h1 className="text-red-500">Hello World</h1>
             <Desc />
             <button>HAHA</button>
         </div>
